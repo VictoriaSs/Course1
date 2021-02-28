7f765060123a4f8ca33fbed07ec4d356a77ff036
Вика <p.viktoria@mail.ru>
2021-02-28 09:49:27 +0300

    Renew

--- src/main/java/Programm.java
+++ src/main/java/Programm.java
@@ -10,17 +10,18 @@ public class Programm {
     }
 
 
+
     public static void main(String[] args) {
 
         Enum[] rights = {Rights.REVIER, Rights.MODERATOR, Rights.ADMIN, Rights.USER};
         for (Enum r : rights) {
-            if (Rights.Rights.USER.equals(r)) {
+            if (Rights.USER.equals(r)) {
                 System.out.println("NO connection");
-            } else if (Rights.Rights.ADMIN.equals(r)) {
+            } else if (Rights.ADMIN.equals(r)) {
                 System.out.println("NO connection");
-            } else if (Rights.Rights.MODERATOR.equals(r)) {
+            } else if (Rights.MODERATOR.equals(r)) {
                 System.out.println("Conection -Yes!");
-            } else if (Rights.Rights.REVIER.equals(r)) {
+            } else if (Rights.REVIER.equals(r)) {
                 System.out.println("Conection -Yes!");
             }
 
